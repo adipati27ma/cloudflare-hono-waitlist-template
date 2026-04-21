1219f1824be61ab5d3d51defcea302c4a4e3af85
adipati27ma <adipati.cs.samsung@gmail.com>
2026-04-21 18:56:10 +0700

    feat: test new command, setup cloudflare access

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import { accessAuth } from "./middleware/auth";
 const app = new Hono();
 
 // running locally using Wrangler (simulate running in Cloudflare Workers)
 // Wrangler need configuration file
-app.get("/api/health", (c) => c.json("Healthy!🔥"));
+// Wrangler also can be used to deploy the app to Cloudflare Workers
+app.use(accessAuth).get("/api/health", (c) => c.json("Healthy!🔥"));
 
 export default app;
